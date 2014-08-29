73efa0866292f72701fc1031ff44c58c0f171135
JiHyun.Song <jihyun.song0829@gmail.com>
2014-08-29 13:24:26 +0900

    message identify from body color to text color

--- app/src/main/java/com/secsm/keepongoing/Shared/MyVolley.java
+++ app/src/main/java/com/secsm/keepongoing/Shared/MyVolley.java
@@ -28,10 +28,10 @@ public class MyVolley {
 
     public static void init(Context context) {
         mContext = context;
-        mRequestQueue = Volley.newRequestQueue(context);
-//        ImprovedDiskBasedCache cache = new ImprovedDiskBasedCache(context.getCacheDir(), 16 * 1024 * 1024);
-//
-//        mRequestQueue = new RequestQueue(cache, new BasicNetwork(new HurlStack()));
+//        mRequestQueue = Volley.newRequestQueue(context);
+        ImprovedDiskBasedCache cache = new ImprovedDiskBasedCache(context.getCacheDir(), 16 * 1024 * 1024);
+
+        mRequestQueue = new RequestQueue(cache, new BasicNetwork(new HurlStack()));
         mRequestQueue.start();
         int memClass = ((ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE))
                 .getMemoryClass();
@@ -45,9 +45,9 @@ public class MyVolley {
         if (mRequestQueue != null) {
             return mRequestQueue;
         } else {
-            mRequestQueue = Volley.newRequestQueue(context);
-//            ImprovedDiskBasedCache cache = new ImprovedDiskBasedCache(context.getCacheDir(), 16 * 1024 * 1024);
-//            mRequestQueue = new RequestQueue(cache, new BasicNetwork(new HurlStack()));
+//            mRequestQueue = Volley.newRequestQueue(context);
+            ImprovedDiskBasedCache cache = new ImprovedDiskBasedCache(context.getCacheDir(), 16 * 1024 * 1024);
+            mRequestQueue = new RequestQueue(cache, new BasicNetwork(new HurlStack()));
             mRequestQueue.start();
             return mRequestQueue;
 //            throw new IllegalStateException("RequestQueue not initialized");
