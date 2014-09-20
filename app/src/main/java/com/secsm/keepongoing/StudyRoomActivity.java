d5f54ccd0e6e739693587cb9e7ff3a6933caaf1a
JiHyun.Song <jihyun.song0829@gmail.com>
2014-09-20 21:18:05 +0900

    delete comments

--- app/src/main/java/com/secsm/keepongoing/StudyRoomActivity.java
+++ app/src/main/java/com/secsm/keepongoing/StudyRoomActivity.java
@@ -543,87 +543,6 @@ public class StudyRoomActivity extends BaseActivity {
         }
     }
 
-
-//    //@민수 통신
-//    private void acheivetimeRegisterRequest(String target_time,String accomplished_time,String date) {
-//        target_time = target_time.trim().replace(" ", "%20");
-//        accomplished_time = accomplished_time.trim().replace(" ", "%20");
-//        date = date.trim().replace(" ", "%20");
-//
-//        final String temp_target_time=target_time;
-//        final String temp_accomplished_time=accomplished_time;
-//        final String temp_date=date;
-//        String get_url = KogPreference.REST_URL +
-//                "Time"; //+
-////                "?nickname=" + KogPreference.getNickName(StudyRoomActivity.this) +
-////                "&target_time=" + target_time +
-////                "&accomplished_time=" + accomplished_time+
-////                "&date=" + date;
-//        JSONObject sendBody = new JSONObject();
-//        try{
-//            sendBody.put("nickname", KogPreference.getNickName(StudyRoomActivity.this));
-//            sendBody.put("target_time", target_time);
-//            sendBody.put("accomplished_time", accomplished_time);
-//            sendBody.put("date", date);
-//        }catch (JSONException e)
-//        {
-//            Log.e(LOG_TAG, " sendBody e : " + e.toString());
-//        }
-//
-//
-//
-////http://210.118.74.195:8080/KOG_Server_Rest/rest/Time?nickname=jins&target_time=10:00:00&accomplished_time=00:00:00&date=2014/8/25
-//
-//        Log.i(LOG_TAG, "get_url : " + get_url);
-//
-//        JsonObjectRequest jsObjRequest = new JsonObjectRequest(Request.Method.POST, get_url, sendBody,
-//                new Response.Listener<JSONObject>() {
-//                    @Override
-//                    public void onResponse(JSONObject response) {
-//                        Log.i(LOG_TAG, "POST JSONObject");
-//                        Log.i(LOG_TAG, response.toString());
-//
-//                        try {
-//                            int status_code = response.getInt("status");
-//                            if (status_code == 200) {
-//                                String rMessage = response.getString("message");
-//                                // real action
-//                                // GoNextPage();
-//                                Toast.makeText(getBaseContext(),"시간등록 완료"+temp_accomplished_time, Toast.LENGTH_SHORT).show();
-//                            } else if (status_code == 9001) {
-//
-//                                Toast.makeText(getBaseContext(), "시간등록이 불가능합니다.", Toast.LENGTH_SHORT).show();
-//                            }
-//                            else if(status_code ==1001) {
-//                                acheivetimeputRequest(temp_target_time, temp_accomplished_time, temp_date);
-//                            }
-//                            else {
-//                                Toast.makeText(getBaseContext(), "통신 장애", Toast.LENGTH_SHORT).show();
-//                                if (KogPreference.DEBUG_MODE) {
-//                                    Toast.makeText(getBaseContext(), LOG_TAG + response.getString("message"), Toast.LENGTH_SHORT).show();
-//                                }
-//                            }
-//                        } catch (Exception e) {
-//                        }
-//                    }
-//                }, new Response.ErrorListener() {
-//            @Override
-//            public void onErrorResponse(VolleyError error) {
-//                Log.i(LOG_TAG, "Response Error");
-//                Toast.makeText(getBaseContext(), "통신 장애", Toast.LENGTH_SHORT).show();
-//                if (KogPreference.DEBUG_MODE) {
-//                    Toast.makeText(getBaseContext(), LOG_TAG + " - Response Error", Toast.LENGTH_SHORT).show();
-//                }
-//            }
-//        }
-//        );
-//
-//        vQueue.add(jsObjRequest);
-//        vQueue.start();
-//
-//    }
-
-
     /** 타이머 종료시 서버에 시간 전송 Post */
     private void achievetimeRegisterRequest(String target_time,String accomplished_time,String date) {
         target_time = target_time.trim().replace(" ", "%20");
@@ -685,67 +604,6 @@ public class StudyRoomActivity extends BaseActivity {
         }
     }
 
-    //@통신
-//    private void acheivetimeputRequest(String target_time,String accomplished_time,String date) {
-//        String get_url = KogPreference.REST_URL +
-//                "Time"; // +
-////                "?nickname=" + KogPreference.getNickName(StudyRoomActivity.this) +
-////                "&target_time=" + target_time +
-////                "&accomplished_time=" + accomplished_time+
-////                "&date=" + date;
-//
-//        JSONObject sendBody = new JSONObject();
-//        try{
-//            sendBody.put("nickname", KogPreference.getNickName(StudyRoomActivity.this));
-//            sendBody.put("target_time", target_time);
-//            sendBody.put("accomplished_time", accomplished_time);
-//            sendBody.put("date", date);
-//        }catch (JSONException e)
-//        {
-//            Log.e(LOG_TAG, " sendBody e : " + e.toString());
-//        }
-//
-//
-//        Log.i(LOG_TAG, "get_url : " + get_url);
-//
-//        JsonObjectRequest jsObjRequest = new JsonObjectRequest(Request.Method.PUT, get_url, null,
-//                new Response.Listener<JSONObject>() {
-//                    @Override
-//                    public void onResponse(JSONObject response) {
-//                        Log.i(LOG_TAG, "get JSONObject");
-//                        Log.i(LOG_TAG, response.toString());
-//
-//                        try {
-//                            int status_code = response.getInt("status");
-//                            if (status_code == 200) {
-//                                String rMessage = response.getString("message");
-//                                // real action
-//                                // GoNextPage();
-////                                Toast.makeText(getBaseContext(), LOG_TAG +rMessage, Toast.LENGTH_SHORT).show();
-//                            } else if (status_code == 9001) {
-//                                Toast.makeText(getBaseContext(), "시간등록이 불가능합니다. PUT", Toast.LENGTH_SHORT).show();
-//                            } else {
-//                                Toast.makeText(getBaseContext(), "통신 장애", Toast.LENGTH_SHORT).show();
-//                                if (KogPreference.DEBUG_MODE) {
-//                                    Toast.makeText(getBaseContext(), LOG_TAG + response.getString("message"), Toast.LENGTH_SHORT).show();
-//                                }
-//                            }
-//                        } catch (Exception e) {
-//                        }
-//                    }
-//                }, new Response.ErrorListener() {
-//            @Override
-//            public void onErrorResponse(VolleyError error) {
-//                Log.i(LOG_TAG, "Response Error");
-//                Toast.makeText(getBaseContext(), "통신 장애", Toast.LENGTH_SHORT).show();
-//                if (KogPreference.DEBUG_MODE) {
-//                    Toast.makeText(getBaseContext(), LOG_TAG + " - Response Error", Toast.LENGTH_SHORT).show();
-//                }
-//            }
-//        }
-//        );
-//        vQueue.add(jsObjRequest);
-//    }
     /** 타이머 종료시 서버에 시간 전송 Put */
     private void achievetimeputRequest(String target_time,String accomplished_time,String date) {
         final String temp_accomplished_time=accomplished_time;
@@ -1753,19 +1611,6 @@ public class StudyRoomActivity extends BaseActivity {
         // TODO : check _time
         Log.i(LOG_TAG, "execSQL : " + query);
         db.execSQL(query);
-//        db.execSQL("INSERT INTO Chat " +
-//                //"(room_id, senderID, senderText, year, month, day, time, me) " +
-//                "(rid, senderID, senderText, year, month, day, me, messageType) " +
-//                "VALUES (" +
-//                "'" + rID + "','"
-//                + _senderID + "','"
-//                + _senderText + "','"
-//                + year + "','"
-//                + month + "','"
-//                + day + "','"
-//                //+ _time + "','"
-//                + _me + "','"
-//                + _messageType + " ');");
         db.close();
         mDBHelper.close();
     }
@@ -2092,50 +1937,6 @@ public class StudyRoomActivity extends BaseActivity {
         } catch (IOException e) {
             e.printStackTrace();
         }
-//        String get_url = KogPreference.REST_URL
-//                + "Socket"
-//                + "?previousip=" + Encrypt.encodeIfNeed(KogPreference.getNickName(StudyRoomActivity.this));
-//
-//        Log.i(LOG_TAG, "URL : " + get_url);
-//
-//        JsonObjectRequest jsObjRequest = new JsonObjectRequest(Request.Method.GET, Encrypt.encodeIfNeed(get_url), null,
-//                new Response.Listener<JSONObject>() {
-//                    @Override
-//                    public void onResponse(JSONObject response) {
-//                        Log.i(LOG_TAG, "get JSONObject");
-//                        Log.i(LOG_TAG, response.toString());
-//
-//                        try {
-//                            int status_code = response.getInt("status");
-//                            if (status_code == 200) {
-//                                Log.i(LOG_TAG, "Disconnection succeed");
-//                            }
-//                            else {
-//                                Log.i(LOG_TAG, "Disconnection failed");
-//                                Toast.makeText(getBaseContext(), "?µì‹  ?ëŸ¬ : \n?œë²„?€???°ê²°??ëª¨í˜¸?©ë‹ˆ??", Toast.LENGTH_SHORT).show();
-//                                if (KogPreference.DEBUG_MODE) {
-//                                    Toast.makeText(getBaseContext(), LOG_TAG + response.getString("message"), Toast.LENGTH_SHORT).show();
-//                                }
-//                            }
-//                        }
-//                        catch (Exception e) {
-//                            Log.i(LOG_TAG, "Disconnection error");
-//                        }
-//                    }
-//                },
-//                new Response.ErrorListener() {
-//                    @Override
-//                    public void onErrorResponse(VolleyError error) {
-//                        Toast.makeText(getBaseContext(), "?µì‹  ?ëŸ¬ : \nError on disconnecting socket", Toast.LENGTH_SHORT).show();
-//                        Log.i(LOG_TAG, "Disconnection Response Error");
-//                        if (KogPreference.DEBUG_MODE) {
-//                            Toast.makeText(getBaseContext(), LOG_TAG + " - Response Error", Toast.LENGTH_SHORT).show();
-//                        }
-//                    }
-//                });
-//
-//        vQueue.add(jsObjRequest);
-//        vQueue.start();
     }
 
     public String getRealDate() {
