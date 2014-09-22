c501c63662354d03ab8a661dd58c7865fec3276e
MinSu Kim <bluetiger20@naver.com>
2014-09-22 23:48:37 +0900

    minsu toss to jins

--- app/src/main/java/com/secsm/keepongoing/Quiz/CustomOnItemSelectedListener.java
+++ app/src/main/java/com/secsm/keepongoing/Quiz/CustomOnItemSelectedListener.java
@@ -7,14 +7,13 @@ package com.secsm.keepongoing.Quiz;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemSelectedListener;
-import android.widget.Toast;
 
 public class CustomOnItemSelectedListener implements OnItemSelectedListener {
 
     public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
-        Toast.makeText(parent.getContext(),
+/*        Toast.makeText(parent.getContext(),
                 "OnItemSelectedListener : " + parent.getItemAtPosition(pos).toString(),
-                Toast.LENGTH_SHORT).show();
+                Toast.LENGTH_SHORT).show();*/
     }
 
     @Override
