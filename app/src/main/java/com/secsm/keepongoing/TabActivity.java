7746b6215813561bac6697feca6f9c28e4a79f9c
JiHyun.Song <jihyun.song0829@gmail.com>
2014-10-02 17:01:19 +0900

    bug fixed -- achievetime initializing by minsu

--- app/src/main/java/com/secsm/keepongoing/TabActivity.java
+++ app/src/main/java/com/secsm/keepongoing/TabActivity.java
@@ -321,10 +321,14 @@ public class TabActivity extends BaseActivity implements View.OnClickListener {
 
         }
         //성취시간 초기화
-
-
-        ahcieve_time.setText(timediff(TabActivity.this));
-        ahcieve_time_sec.setText( "."   + (achieve_Seconds / 10 == 0 ? "0" + achieve_Seconds : achieve_Seconds));
+        if(Preference.getBoolean(TabActivity.this, "firsttogglebollean")){
+            ahcieve_time.setText("00:00");
+            ahcieve_time_sec.setText(".00");
+        }
+        else{
+            ahcieve_time.setText(timediff(TabActivity.this));
+            ahcieve_time_sec.setText( "."   + (achieve_Seconds / 10 == 0 ? "0" + achieve_Seconds : achieve_Seconds));
+        }
         Preference.setString(TabActivity.this, "achieve_time", ahcieve_time.getText().toString()+":"+ahcieve_time_sec.getText().toString().substring(1,3));
         _current_Time.setText(minusgoalachieve());
         currenttime2_sec.setText( "." + (s/ 10 == 0 ? "0" + s : s));
